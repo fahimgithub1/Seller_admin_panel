b7719604a99ba58137b48be14576139af49f6410
Dms Fahim Patoawary <dmsfahim@gmail.com>
2022-11-15 10:00:22 +0600

    df

---
+++ asset/js/main.js
@@ -0,0 +1,8 @@
+
+
+const nevClick = document.getElementById("TopIcon");
+
+nevClick.addEventListener('click',()=>{
+    const leftitem = document.querySelector(".LeftSudeNav");
+    leftitem.style.display = "block";
+})
