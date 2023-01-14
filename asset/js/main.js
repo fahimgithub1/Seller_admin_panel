9bfdd38131b71fa1ab373d7cf2a2f3bd2634750d
Dms Fahim Patoawary <dmsfahim@gmail.com>
2023-01-14 16:07:04 +0600

    14/1/2023

--- asset/js/main.js
+++ asset/js/main.js
@@ -1,8 +1,23 @@
 
 
 const nevClick = document.getElementById("TopIcon");
-
 nevClick.addEventListener('click',()=>{
     const leftitem = document.querySelector(".LeftSudeNav");
     leftitem.style.display = "block";
 })
+
+
+// add new users
+// const addnewUser = document.getElementById("addnewUser");
+function addnewUser(){
+    document.querySelector('.ManageUsers').style.display = "none";
+    document.querySelector(".addUser").style.display = "block";
+};
+
+// add new  save
+// const userSave = document.getElementById("userSave");
+function userSave(){
+    document.querySelector('.ManageUsers').style.display = "block";
+    document.querySelector(".addUser").style.display = "none";
+}
+
